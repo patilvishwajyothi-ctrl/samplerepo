7c7b7866f7ddc68a29a32e733d6aed0999339eab
patilvishwajyothi-ctrl <patilvishwajyothi@gmail.com>
2026-01-21 14:56:17 +0530

    sample project push

---
+++ src/test/java/pc1/pgm1_Test.java
@@ -0,0 +1,15 @@
+package pc1;
+
+import org.testng.annotations.Test;
+
+public class pgm1_Test {
+	@Test
+	public void method1_Test() {
+		System.out.println("im method m1");
+	}
+	@Test
+	public void method2_Test() {
+		System.out.println("im method m2");
+	}
+
+}
---
+++ src/test/java/pc2/pgm2_Test.java
@@ -0,0 +1,16 @@
+package pc2;
+
+import org.testng.annotations.Test;
+
+public class pgm2_Test {
+	@Test
+	public void method3_Test() {
+		System.out.println("im method m3");
+	}
+	@Test
+	public void method4_Test() {
+		System.out.println("im method m4");
+	}
+
+
+}
