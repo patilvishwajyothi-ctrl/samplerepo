55d385bbfc6f7859ce6f2b93fe87271fe5a797f2
patilvishwajyothi-ctrl <patilvishwajyothi@gmail.com>
2026-01-22 12:19:11 +0530

    PARAMETER

--- src/test/java/pc1/pgm1_Test.java
+++ src/test/java/pc1/pgm1_Test.java
@@ -5,6 +5,14 @@ import org.testng.annotations.Test;
 public class pgm1_Test {
 	@Test(groups="RT")
 	public void method1_Test() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 		System.out.println("im method m1");
 	}
 	@Test(groups="ST")
--- src/test/java/pc2/pgm2_Test.java
+++ src/test/java/pc2/pgm2_Test.java
@@ -5,6 +5,15 @@ import org.testng.annotations.Test;
 public class pgm2_Test {
 	@Test(groups="ST")
 	public void method3_Test() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
 		System.out.println("im method m3");
 	}
 	@Test(groups="RT")
